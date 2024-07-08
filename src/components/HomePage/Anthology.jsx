f9f2d316c131ea7c5130bb7fa035be3c361d3da4
Mason-Zeng <masonzeng123@gmail.com>
2024-07-08 14:49:28 -0400

    added statement of faith and also started with anthology

--- src/components/HomePage/Anthology.jsx
+++ src/components/HomePage/Anthology.jsx
@@ -1,9 +1,26 @@
-
-
+import React from "react";
+import '../static/Anthology.css';
 const Anthology = () => {
     return (
         <section className='projectsSection' id='projects' >
-            
+        <div className="anthologySummary">
+            <h2 style={{textAlign:"center"}}>Our Anthology</h2>
+            <div id='book'>
+                <a href="http://bccc-ny.org/assets/pdf/SBI7Anthology.pdf">
+                    <img src= "../images/book.png" alt="book image"/>
+                </a>
+            </div>
+            <p>The following anthology was created in the summer of 2023 by SBI (Summa Bible Institute) 7.0, a youth ministry team from Brooklyn Community Christian Church.</p>
+            <p>Our goal is to model ourselves after 1 Timothy 4:12:</p>
+            <p class="alignAntho">Let no one despise you for your youth, but set the believers an example in speech, in conduct, in love, in faith, in purity.</p>
+            <p>
+                Each SBI member has written their testimony, a story detailing their experiences with God and their acceptance of Jesus Christ as their Lord and Savior. Along with 
+                their testimony, we have each submitted creative works which serve as expressions of our faith in Jesus. God has done so much in our lives, and we only wish to share 
+                it with the world. We're hoping you will be inspired and moved by these works.
+            </p>
+            <p class="alignAntho">SBI 7.0 Directors<br /> Cullen Ye and Simeon Leung</p>
+
+        </div>
         </section>
     );
 }
--- src/components/HomePage/StatementOfFaith.jsx
+++ src/components/HomePage/StatementOfFaith.jsx
@@ -7,9 +7,16 @@ const StatementOfFaith = () => {
             <div className='aboutSummary'>
                 <h2 style={{textAlign:"center"}}>Statement Of Faith </h2>
                 <ol>
-                    <li>Coffee</li>
-                    <li>Tea</li>
-                    <li>Milk</li>
+                    <li>We believe that all Scripture, consisting of 66 books in the Old and New Testament, are the only divinely inspired infallible words of God. Scripture is the supreme authority in all matters of faith and conduct.</li>
+                    <li>We believe that there is one living and true God, eternally existing as three Persons, the Father, the Son and the Holy Spirit, equal in power and glory; that this triune God created all, upholds all and governs all.</li>
+                    <li>We believe that Jesus Christ our Lord is the Son of God - both fully God and fully man, the Savior of the world, and that through His life, death and resurrection an atonement was made for sin and redemption was provided for mankind.</li>
+                    <li>We believe that the repentance of sin and the receiving of Jesus Christ as personal Savior is the one and only way whereby sinful man can inherit eternal life.</li>
+                    <li>We believe that the Holy Spirit, the Third Person of the blessed Trinity brings people into a saving relation with God, and that He is the Comforter and Guide of all who receive Jesus Christ as personal Savior.</li>
+                    <li>We believe that man was created in the image of God for the purpose to glorify God. We believe as the result of the fall, sin entered as a universal condition to the human race and that sin condemned the whole human race to death.</li>
+                    <li>We believe that at the end of the age, the Lord Jesus Christ will descend from Heaven to redeem his people into eternal life, and to judge the wicked into eternal death.</li>
+                    <li>We believe that the Church is the body of believers united in Christ with Christ as the head. The church invisible and universal is composed of the redeemed of all ages. The local church is the visible manifestation of the invisible church.</li>
+                    <li>We believe baptism by water is the public confession of Christ as Savior and Lord. It is a symbol of one's union by faith with Christ in death, burial and resurrection.</li>
+                    <li>We believe that the Lord's supper should be observed by all. It consists of partaking corporately the bread and wine; symbolizing the death of Christ for the remission of sins, and our continual dependence upon Him for our sustenance.</li>
                 </ol>
             </div>
         </section>
