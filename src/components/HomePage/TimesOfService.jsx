00c54c30b8ca15ed8f7f8d1c277b92924779ca80
Mason-Zeng <masonzeng123@gmail.com>
2024-07-11 02:10:14 -0400

    centered the times of service

--- src/components/HomePage/TimesOfService.jsx
+++ src/components/HomePage/TimesOfService.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../static/TOS.css';
 
 
 const StatementOfFaith = () => {
@@ -6,38 +7,40 @@ const StatementOfFaith = () => {
         <section className='darkSection'>
             <div className='aboutSummary'>
                 <h2 style={{textAlign:"center"}}>Service Times</h2>
-                <table >
-                    <tr>
-                        <th>Service	</th>
-                        <th>Day</th>
-                        <th>Time</th>
-                    </tr>
-                    <tr>
-                        <td>Chinese Service</td>
-                        <td>Sunday	</td>
-                        <td>9:30 am</td>
-                    </tr>
-                    <tr>
-                        <td>English Service	</td>
-                        <td>Sunday	</td>
-                        <td>11:30 am</td>
-                    </tr>
-                    <tr>
-                        <td>English Prayer Meeting</td>
-                        <td>Tuesday</td>
-                        <td>7:30 PM</td>
-                    </tr>
-                    <tr>
-                        <td>Chinese Prayer Meeting	</td>
-                        <td>Wednesday	</td>
-                        <td>8:00PM</td>
-                    </tr>
-                    <tr>
-                        <td>English/Chinese Fellowship	</td>
-                        <td>Friday	</td>
-                        <td>7:30 PM</td>
-                    </tr>
-                </table>
+                <div id="table">
+                    <table>
+                        <tr>
+                            <th>Service	</th>
+                            <th>Day</th>
+                            <th>Time</th>
+                        </tr>
+                        <tr>
+                            <td>Chinese Service</td>
+                            <td>Sunday	</td>
+                            <td>9:30 am</td>
+                        </tr>
+                        <tr>
+                            <td>English Service	</td>
+                            <td>Sunday	</td>
+                            <td>11:30 am</td>
+                        </tr>
+                        <tr>
+                            <td>English Prayer Meeting</td>
+                            <td>Tuesday</td>
+                            <td>7:30 PM</td>
+                        </tr>
+                        <tr>
+                            <td>Chinese Prayer Meeting</td>
+                            <td>Wednesday	</td>
+                            <td>8:00PM</td>
+                        </tr>
+                        <tr>
+                            <td>English/Chinese Fellowship</td>
+                            <td>Friday</td>
+                            <td>7:30 PM</td>
+                        </tr>
+                    </table>
+                    </div>
             </div>
         </section>
     );
