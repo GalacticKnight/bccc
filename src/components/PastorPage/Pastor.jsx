8ebed6766e42daa900ea35499b54a4b071766c00
Mason-Zeng <masonzeng123@gmail.com>
2024-07-16 12:04:10 -0400

    added short description for pastor page

--- src/components/PastorPage/Pastor.jsx
+++ src/components/PastorPage/Pastor.jsx
@@ -7,6 +7,14 @@ const Pastor = () => {
         <section className='darkSection' id='about'>
                 <h3 className='siteHeadings'>Pastor Andy Tso</h3>
                 <img id = "pastor-image"src={pastorImage} alt="pastor image"/>
+                <div className = "aboutSummary">
+                    <p> Andy is the Lead Pastor of the Brooklyn Community Christian Church, where he first attended 1974 and baptized 1977. 
+                        He preaches in both English and Chinese Worship services. He graduated from NYU (1983) and majored in Economics and 
+                        Pre-Med, with Minors in Chinese History and Chemistry. He received his JD from Univ Idaho, and MDiv from Gordon-Conwell 
+                        Theological Seminary.
+                    </p>
+                    <p>He and his wife Hilary left promising careers in law and the university to answer God's call to full-time ministry.</p>
+                </div>
         </section>
     );
 }
