e0154b2af35869fb14af09c0fca35a44f8352d4a
Vincent Lau <vincent.lau.cs@gmail.com>
2024-07-16 19:34:14 -0400

    remove nav buttons

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -16,7 +16,7 @@ const NavBar = () => {
                 </button>
                 <div class="collapse navbar-collapse" id="navbarExample01">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item active">
+                        {/* <li class="nav-item active">
                             <a class="nav-link" aria-current="page" href="#hero">Home</a>
                         </li>
                         <li class="nav-item">
@@ -30,6 +30,9 @@ const NavBar = () => {
                         </li>
                         <li class="nav-item active">
                             <a class="nav-link" aria-current="page" href="#time">Times Of Service</a>
+                        </li> */}
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="#about" >About</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" aria-current="page" href="#Anthology" >Anthology</a>
