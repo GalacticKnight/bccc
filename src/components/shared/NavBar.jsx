753c15cec5dde1f77d1f76232570898fcbae9a44
Mason-Zeng <masonzeng123@gmail.com>
2024-07-29 20:55:03 -0400

    added mobile nav bar

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,58 +1,52 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
-import {HashLink} from 'react-router-hash-link';
+import React, { useState } from 'react';
+import { HashLink } from 'react-router-hash-link';
 import '../static/Navbar.css'
 const Logo = require('../images/BCCC_logo.png');
 
-
 const NavBar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const navItems = [
+        { to: '/#hero', text: 'Home' },
+        { to: '/#SOF', text: 'Our Beliefs' },
+        { to: '/#anthology', text: 'Anthology' },
+        { to: '/#footer', text: 'Socials' }
+    ];
+
     return (
-        <nav class="navbar navbar-expand-lg fixed-top navbar-scroll" data-mdb-navbar-init>
-            <div class="container-fluid">
-                <img src={Logo}/>
-                <button class="navbar-toggler ps-0" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarExample01" aria-controls="navbarExample01"  aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="d-flex justify-content-start align-items-center">
-                        <i class="fas fa-bars"></i>
-                    </span>
+        <nav className="navbar navbar-expand-lg fixed-top navbar-scroll">
+            <div className="container-fluid">
+                <img src={Logo} alt="Logo"/>
+                <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+                    <span className="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarExample01">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item active">
-                            <HashLink smooth to ='/#hero' class="nav-link" aria-current="page" href="#">Home</HashLink>
-                        </li>
-                        <li class="nav-item active">
-                            <HashLink smooth to ='/#SOF' class="nav-link" aria-current="page" href="#">Our Beliefs</HashLink>
-                        </li>
-                        <li class="nav-item">
-                            <HashLink smooth to ='/#anthology' class="nav-link" aria-current="page" href="#" >Anthology</HashLink>
-                        </li>
-                        <li class="nav-item">
-                            <HashLink smooth to ='/#footer' class="nav-link" aria-current="page" href="#">Socials</HashLink >
-                        </li>
+                <div className={`navbar-collapse ${isOpen ? 'show' : ''}`}>
+                    <ul className="navbar-nav">
+                        {navItems.map((item, index) => (
+                            <li 
+                                className="nav-item" 
+                                key={index}
+                                style={{transitionDelay: `${index * 0.1}s`}}
+                            >
+                                <HashLink 
+                                    smooth 
+                                    to={item.to} 
+                                    className="nav-link"
+                                    onClick={toggleMenu}
+                                >
+                                    {item.text}
+                                </HashLink>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
         </nav>
-
-        
     );
 }
 
-
 export default NavBar;
-{/*         // <header className='navbar navbar-expand-md bg-white navbar-white'>
-        //     <div className='navBar container-fluid px-5'>
-        //         <img src={Logo} className='logo'/>
-        //         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#main-nav" aria-controls="main-nav" aria-expanded="false" aria-label="Toggle navigation">
-        //             <span className="navbar-toggler-icon"></span>
-        //         </button>
-        //         <div className='collapse navbar-collapse justify-content-end align-center' id='main-nav'>
-        //             <ul className='navbar-nav'>
-        //                 <li className='nav-item'><HashLink smooth to='/#about' className='nav-link' href="#">About</HashLink></li>
-        //                 <li className='nav-item'><HashLink smooth to='/#anthology' className='nav-link' href="#">Anthology</HashLink></li>
-        //                 <li className='nav-item'><Link to='/contact' className='nav-link'>Contact</Link></li>
-        //                 <li className='nav-item'><HashLink smooth to='/#anthology' className='nav-link' href="#">Anthology</HashLink></li>
-        //             </ul>
-        //         </div>
-        //     </div>
-        // </header> */}
